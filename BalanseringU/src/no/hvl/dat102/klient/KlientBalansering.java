89fe38b79e83ba94b45d6402688b20e43f3b95ef
Stian Grønås <42640705+hvl578007@users.noreply.github.com>
2019-03-05 12:42:36 +0100

    "final ver"

--- BalanseringU/src/no/hvl/dat102/klient/KlientBalansering.java
+++ BalanseringU/src/no/hvl/dat102/klient/KlientBalansering.java
@@ -1,13 +1,19 @@
 package no.hvl.dat102.klient;
 
 import no.hvl.dat102.Balansering;
+import java.util.Scanner;
 
 public class KlientBalansering{
      public static void main(String[] args){
-        final String filnavn = "data.txt";
-        //Leser inn en tekst fra fil
-        Balansering balansering = new Balansering();
-        balansering.lesFraFil(filnavn);
+        //final String filnavn = "data.txt";
+         Scanner lesar = new Scanner(System.in);
+         System.out.println("Skriv inn filnamn:");
+         String filnavn = lesar.nextLine();
+         lesar.close();
+
+         //Leser inn en tekst fra fil
+         Balansering balansering = new Balansering();
+         balansering.lesFraFil(filnavn);
  }//main
 
 }//class
